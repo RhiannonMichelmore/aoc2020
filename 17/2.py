b62574fca1a9918d87664947cdea331461eee7e7
Rhiannon Michelmore <rhiannon.michelmore@gmail.com>
2020-12-17 17:21:44 +0000

    rewrote part 2 day 17 to be for any n

--- 17/2.py
+++ 17/2.py
@@ -1,115 +1,116 @@
 import sys
+import itertools
+import copy
+
+def print_nd(state,n):
+    print()
+    for k in sorted(state.keys()):
+        print('Dims =',k)
+        grid = state[k]
+        for row in grid:
+            print(''.join(row))
+
+        print()
+    print('------------------')
+    
 
 def main(in_string):
-    z0 = list(map(list,in_string.split('\n')))
+    zero = list(map(list,in_string.split('\n')))
+    nd = 4
+    # minus 2 as our input is always 2d
+    state = dict()
+    zero_key = ''
+    dmin = 0
+    dmax = 0
 
-    state = [[z0]]
-    # state[w][z][y][x]
+    for i in range(nd-2):
+        if i == nd-3:
+            zero_key += '0'
+        else:
+            zero_key += '0,'
+    state[zero_key] = zero
 
     print('------------------')
-    print_4d(state)
+    print_nd(state,nd)
     
     for i in range(6):
-        # expand
-        for w,grid_3d in enumerate(state):
-            for z,grid_2d in enumerate(grid_3d):
-                new_col_no = len(grid_2d[0])+2
-                new_2d = [['.' for c in range(new_col_no)]]
-                for r in grid_2d:
-                    new_r = ['.'] + r + ['.']
-                    new_2d.append(new_r)
-                new_2d.append(['.' for c in range(new_col_no)])
-                state[w][z] = new_2d
+        # expand existing
+        for k in state.keys():
+            new_col_no = len(state[k][0])+2
+            new_2d = [['.' for c in range(new_col_no)]]
+            for row in state[k]:
+                new_row = ['.'] + row + ['.']
+                new_2d.append(new_row)
+            new_2d.append(['.' for c in range(new_col_no)])
+            state[k] = new_2d
 
-        
 
-        size = (len(state[0][0]),len(state[0][0][0]))
-        for w,grid_3d in enumerate(state):
-            lg = [['.' for c in range(size[0])] for r in range(size[1])]
-            hg = [['.' for c in range(size[0])] for r in range(size[1])]
-            state[w].insert(0,lg)
-            state[w].append(hg)
+        # add new
+        size = (len(state[list(state.keys())[0]]),len(state[list(state.keys())[0]][0]))
+        empty_grid = [['.' for c in range(size[0])] for r in range(size[1])]
 
-        z_size = len(state[0])
-        lg = [[['.' for c in range(size[0])] for r in range(size[1])] for f in range(z_size)]
-        hg = [[['.' for c in range(size[0])] for r in range(size[1])] for f in range(z_size)]
-        state.insert(0,lg)
-        state.append(hg)
+        dmin -= 1
+        dmax += 1
+        combs = list(itertools.product(list(range(dmin,dmax+1)),repeat=nd-2))
+        for c in combs:
+            new_key = ','.join(list(map(str,c)))
+            if not new_key in state:
+                state[new_key] = copy.deepcopy(empty_grid)
 
         # actually do game of life shit
-        new_state = []
-        for w,grid_3d in enumerate(state):
-            new_3d = []
-            for z, grid_2d in enumerate(grid_3d):
-                new_2d = []
-                for y, row in enumerate(grid_2d):
-                    new_row = []
-                    for x,entry in enumerate(row):
-                        ns = get_active_neighbours(x,y,z,w,state)
-                        if entry == '#':
-                            if ns == 2 or ns == 3:
-                                new_entry = '#'
-                            else:
-                                new_entry = '.'
+        new_state = dict()
+        for k in state.keys():
+            new_grid = []
+            grid = state[k]
+            for y,row in enumerate(grid):
+                new_row = []
+                for x,entry in enumerate(row):
+                    ns = get_active_neighbours(x,y,k,state)
+                    if entry == '#':
+                        if ns == 2 or ns == 3:
+                            new_entry = '#'
                         else:
-                            if ns == 3:
-                                new_entry = '#'
-                            else:
-                                new_entry = '.'
-                        new_row.append(new_entry)
-                    new_2d.append(new_row)
-                new_3d.append(new_2d)
-            new_state.append(new_3d)
-
+                            new_entry = '.'
+                    else:
+                        if ns == 3:
+                            new_entry = '#'
+                        else:
+                            new_entry = '.'
+                    new_row.append(new_entry)
+                new_grid.append(new_row)
+            new_state[k] = new_grid
 
         state = new_state
-        print_4d(state)
+
+        print_nd(state,nd)
 
     count = 0
-    for g3 in state:
-        for g2 in g3:
-            for r in g2:
-                for e in r:
-                    if e == '#':
-                        count += 1
+    for k in state.keys():
+        grid = state[k]
+        for row in grid:
+            for entry in row:
+                if entry == '#':
+                    count += 1
 
     print('Active:',count)
 
-def get_active_neighbours(x,y,z,w,state):
+def get_active_neighbours(x,y,k,state):
     ns = 0
-    for dw in range(-1,2):
-        for dz in range(-1,2):
+    extra_ds = list(map(int,k.split(',')))
+    all_ds = [[x-1,x,x+1] for x in extra_ds]
+    combs = list(itertools.product(*all_ds))
+    for c in combs:
+        tmp_key = ','.join(list(map(str,c)))
+        if tmp_key in state.keys():
+            tmp_grid = state[tmp_key]
             for dy in range(-1,2):
                 for dx in range(-1,2):
-                    if not (dw == 0 and dz == 0 and dy == 0 and dx == 0):
-                        if in_limit(x+dx,y+dy,z+dz,w+dw,state):
-                            if state[w+dw][z+dz][y+dy][x+dx] == '#':
+                    if not (tmp_key == k and dx == 0 and dy == 0):
+                        if y+dy >= 0 and y+dy < len(tmp_grid) and x+dx >= 0 and x+dx < len(tmp_grid[0]):
+                            if tmp_grid[y+dy][x+dx] == '#':
                                 ns += 1
 
     return ns
-
-def in_limit(x,y,z,w,state):
-    if w >= 0 and w < len(state) and z >= 0 and z < len(state[0]) and y >= 0 and y < len(state[0][0]) and x >= 0 and x < len(state[0][0][0]):
-        return True
-    else:
-        return False
-
-def print_4d(state):
-    print()
-    for w,grid_3d in enumerate(state):
-        print_3d(w,grid_3d)
-    print('-----------------')
-
-def print_3d(w,grid_3d):
-    for z,grid_2d in enumerate(grid_3d):
-        print_2d(w,z,grid_2d)
-
-def print_2d(w,z,grid):
-    print('z='+str(z),'w='+str(w))
-    for row in grid:
-        print(''.join(row))
-    print()
-
 
 if __name__ == "__main__":
     if len(sys.argv) < 2:
